1a8119aa6ea1b4d1eb1d2f5cf4c09fd47be77fff
Alindel <alindel@mail.ru>
2025-10-28 18:25:58 +0400

    Create games-data.js
    страница с js

---
+++ games-data.js
@@ -0,0 +1,27 @@
+javascript
+const games = [
+    {
+        id: 1,
+        title: "Космический стрелок",
+        author: "Я",
+        authorImage: "assets/authors/me.jpg",
+        screenshot: "assets/screenshots/space.jpg",
+        gameUrl: "games/space-shooter/index.html"
+    },
+    {
+        id: 2, 
+        title: "Лабиринт",
+        author: "Я",
+        authorImage: "assets/authors/me.jpg", 
+        screenshot: "assets/screenshots/maze.jpg",
+        gameUrl: "games/maze/index.html"
+    },
+    {
+        id: 3,
+        title: "Платформер",
+        author: "Я", 
+        authorImage: "assets/authors/me.jpg",
+        screenshot: "assets/screenshots/platformer.jpg",
+        gameUrl: "games/platformer/index.html"
+    }
+];
