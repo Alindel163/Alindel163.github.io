7c6be5a2a51a77bc9ef1ea909d28d79bc54d956b
Alindel <alindel@mail.ru>
2025-11-01 10:50:17 +0400

    Update script.js
    23

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const games = [
     {
         title: "Вертолётик. Шаблон для творческих проектов №1",
         description: "Стреляйте в вражеские вертолёты!",
-        control: "<br>перемещение: стрелки, <br>стрельба: кнопка мыши, <br>прицеливание: двигайте мышь<br>",
+        control: "перемещение: стрелки, <br>стрельба: кнопка мыши, <br>прицеливание: двигайте мышь<br>",
         gameUrl: "games/test-game/helicopter.html",
         image: "games/test-game/logo-helicopter.jpg",
         author: "Александров П.В. преподаватель центра"
@@ -17,7 +17,7 @@ function createGameCard(game) {
             <img src="${game.image}" alt="${game.title}" class="game-image">
             <div class="game-content">
                 <h3>Название: ${game.title}</h3>
-                <h3>Управление: <br>${game.control}</h3>
+                <h3<br>Управление:<br>${game.control}</h3>
                 <p>Описание:${game.description}</p>
                 <p>Автор: ${game.author}</p>
                 <p><strong>Нажмите чтобы играть!</strong></p>
